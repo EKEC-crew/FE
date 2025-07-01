13b90794ad78a9c86f2f10e86650ecb23df598fc
Gamgyul <ryusangwan12@gmail.com>
2025-07-01 11:13:51 +0900

    start: project setting

---
+++ src/pages/auth/signUpPage/complete.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Complete = () => {
+  return <div>회원가입 성공/테스트</div>;
+};
+
+export default Complete;
---
+++ src/pages/searchPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchPage = () => {
+  return <div>검색페이지</div>;
+};
+
+export default SearchPage;
