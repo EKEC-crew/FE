78ff96c5e6e6e5c51598ebc425905d0605c6a97b
dongyeol02 <sprent7@gmail.com>
2025-07-18 17:47:01 +0900

    fix:TopGNB 리팩토링

--- src/components/TopGnb/userMenu.tsx
+++ src/components/TopGnb/userMenu.tsx
@@ -1,29 +1,12 @@
-import { useNavigate } from "react-router-dom";
+// components/TopGNB/UserMenu.tsx
+import ProfileButton from "./ProfileButton";
 import AlarmButton from "./AlarmButton";
 
 export default function UserMenu() {
-  const navigate = useNavigate();
-  const userProfile = "";
   return (
-    <div className="flex items-center space-x-4">
-      {/* 알림 아이콘 */}
+    <div className="relative flex items-center space-x-6 mr-2">
       <AlarmButton />
-      {/* 프로필 이미지 → 마이페이지 이동 */}
-      <button
-        onClick={() => navigate("/myPage")}
-        className="flex items-center space-x-2"
-      >
-        {userProfile ? (
-          <img
-            src={userProfile}
-            alt="프로필"
-            className="w-8 h-8 rounded-full object-cover"
-          />
-        ) : (
-          <div className="w-8 h-8 rounded-full bg-gray-300" />
-        )}
-        <span className="text-sm font-medium">0000님</span>
-      </button>
+      <ProfileButton />
     </div>
   );
 }
