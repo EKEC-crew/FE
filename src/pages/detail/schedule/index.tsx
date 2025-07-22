9bbeb982db99a97ea76243ec242d3a50654d6aad
immhyemi <wendy0802@naver.com>
2025-07-22 10:01:29 +0900

    fix: 빌드 오류 수정

--- src/pages/detail/schedule/index.tsx
+++ src/pages/detail/schedule/index.tsx
@@ -8,7 +8,7 @@ import Post from "../../../components/detail/Schedule/button/post";
 
 function Schedule() {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-100 min-h-screen shadow-none">
       <div className="mt-12">
         <Header />
         <Tabs />
@@ -16,18 +16,14 @@ function Schedule() {
 
       <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-12 xl:px-20 pt-6 space-y-6">
         <Notice />
-
         <div className="shadow-md overflow-hidden bg-white rounded-lg p-6 flex flex-col items-center">
           <Calendar />
-
           <div className="mt-6 w-full">
             <ScheduleList />
           </div>
-
           <div className="mt-4 w-full">
             <Pagination />
           </div>
-
           <div className="mt-4">
             <Post />
           </div>
