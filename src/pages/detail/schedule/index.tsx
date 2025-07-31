174a6405e04f6617c9bec32a53378d3f6efdf360
immhyemi <wendy0802@naver.com>
2025-07-31 10:22:29 +0900

    fix: 충돌 오류 재수정

--- src/pages/detail/schedule/index.tsx
+++ src/pages/detail/schedule/index.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import Header from "../../../components/detail/header";
 import Notice from "../../../components/detail/notice";
 import Tabs from "../../../components/detail/tabs";
-import Calendar from "../../../components/detail/Schedule/Calendar";
+import CrewCalendar from "../../../components/detail/Schedule/Calendar";
 import ScheduleList from "../../../components/detail/Schedule/ScheduleList";
 import Pagination from "../../../components/detail/Schedule/button/pagination";
 import Post from "../../../components/detail/Schedule/button/post";
@@ -19,7 +19,7 @@ function Schedule() {
       <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-12 xl:px-20 pt-6 space-y-6">
         <Notice />
         <div className="shadow-md overflow-hidden bg-white rounded-lg p-6 flex flex-col items-center">
-          <Calendar />
+          <CrewCalendar />
           <div className="mt-6 w-full">
             <ScheduleList />
           </div>
