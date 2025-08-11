11ea5d1cd4c0fd564a925f5b563172ec9f59ffb6
Gamgyul <ryusangwan12@gmail.com>
2025-08-11 17:09:49 +0900

    fix: 제목입력 추가

--- src/components/detail/Schedule/PostForm/TitleInput.tsx
+++ src/components/detail/Schedule/PostForm/TitleInput.tsx
@@ -5,13 +5,18 @@ interface TitleInputProps {
 
 const TitleInput = ({ title = "", setTitle }: TitleInputProps) => {
   return (
-    <input
-      type="text"
-      placeholder="제목을 입력해주세요"
-      value={title}
-      onChange={(e) => setTitle?.(e.target.value)}
-      className="w-full border border-[#5e6068] p-2 rounded mb-4"
-    />
+    <>
+      <div className="font-bold mb-2">
+        제목 입력<span className="text-red-500 text-base">*</span>
+      </div>
+      <input
+        type="text"
+        placeholder="제목을 입력해주세요"
+        value={title}
+        onChange={(e) => setTitle?.(e.target.value)}
+        className="w-full border border-[#5e6068] p-2 rounded mb-4"
+      />
+    </>
   );
 };
 
