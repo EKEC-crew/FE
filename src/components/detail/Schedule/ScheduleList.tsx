5c603ed450f160e6349aa43d50b642100b4200ab
Gamgyul <ryusangwan12@gmail.com>
2025-08-14 23:58:31 +0900

    fix: 날짜 종류 변경

--- src/components/detail/Schedule/ScheduleList.tsx
+++ src/components/detail/Schedule/ScheduleList.tsx
@@ -63,13 +63,11 @@ const ScheduleList = ({
             type={schedule.type === 0 ? "정기" : "번개"}
             title={schedule.title}
             date={(() => {
-              const date = new Date(schedule.createdAt);
+              const date = new Date(schedule.day);
               const year = date.getFullYear();
               const month = String(date.getMonth() + 1).padStart(2, "0");
               const day = String(date.getDate()).padStart(2, "0");
-              const hours = String(date.getHours()).padStart(2, "0");
-              const minutes = String(date.getMinutes()).padStart(2, "0");
-              return `${year}.${month}.${day} ${hours}:${minutes}`;
+              return `${year}.${month}.${day}`;
             })()}
             status={schedule.isApplied ? 1 : 0}
             isNew={false}
